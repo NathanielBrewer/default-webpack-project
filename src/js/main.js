87717ce2ca2ffc6bbcd3d660d1ab56cff811741a
Nathaniel Brewer <nathaniel@onsetcommunication.com>
2022-10-12 21:42:40 -0300

    initial configuration

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+import "../scss/styles.scss";
+
+class App {
+    constructor() {
+        /* Omitted */
+    }
+
+    run() {
+        /* Omitted */
+    }
+}
+
+console.log("Starting application");
+
+let app = new App();
+app.run();
+
+console.log("Application started");
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/main.js',
+  },
+  output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ],
+  }
+};
+
+module.rules = [
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader'
+  },
+  {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  },
+];
